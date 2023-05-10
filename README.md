30c998a8630714280138aa168ef7ebc045ccdd0a
George Tipton <gtip22@gmail.com>
2023-05-10 16:06:41 -0600

    updated searchCode

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# 🐑 Guarded Sheep 🐑 
-Guarded Sheep is a small utility to help discover potential intellectual property or leaked source code on GitHub using GitHub's public APIs. 
+# 🐑 Guarded Sheep 🐑
 
-# Features 
+Guarded Sheep is a small utility to help discover potential intellectual property or leaked source code on GitHub using GitHub's public APIs.
 
-### To Do
+# Features
+
+## To Do
+
+### Features
+
 - [ ] Search GitHub repos based on a series of key terms and criteria
 - [ ] When matches are found, get intel on repo owner
 - [ ] Skim repo for sensitive information
+
+### Scenarios
--- guardedsheep.py
+++ guardedsheep.py
@@ -4,32 +4,32 @@ import json
 
 ACCESS_TOKEN = os.getenv('GITHUB_TOKEN')
 config_file_name = 'config.json'
+# Authenticate
+g = Github(ACCESS_TOKEN)
 
-def searchCode():
-    content_file = ""
-    return content_file
+def searchCode(query):
+    results = []
+    findings = {}
+    code_results = g.search_code(query=query)
+    for content_file in code_results:
+        results.append({"name":content_file.name,"repo":content_file.repository.url})
+    return results
 
-def searchRepos():
+def searchRepos(query):
     repos = ""
     return repos
 
-def main():
-    # Authenticate
-    g = Github(ACCESS_TOKEN)
+def searchUsers(query):
+    results = []
+    return results
 
-    # Test authentication success
-    user = g.get_user()
-    print(user.login)
-    
+def main():    
     config_file = open(config_file_name)
     config_data = json.load(config_file)
-    code_query = config_data['code_query']
 
     # Get repo info based on query
-    repositories = g.search_code(query=code_query)
-    for repo in repositories:
-        owner = repo.repository.owner
-        print(owner.name, owner.location, owner.bio, owner.role, )
+    code_findings = searchCode(config_data['code_query'])
+    print(code_findings)
 
 if __name__ == "__main__":
     main()
