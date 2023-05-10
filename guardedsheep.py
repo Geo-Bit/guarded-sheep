3d0882e5a548e03e69f9063a68dfe76264f13155
George Tipton <gtip22@gmail.com>
2023-05-10 15:00:05 -0600

    basic query functionality

--- guardedsheep.py
+++ guardedsheep.py
@@ -1,11 +1,35 @@
 from github import Github
+import os
+import json
+
+ACCESS_TOKEN = os.getenv('GITHUB_TOKEN')
+config_file_name = 'config.json'
+
+def searchCode():
+    content_file = ""
+    return content_file
 
 def searchRepos():
-    return "Ayo"
+    repos = ""
+    return repos
 
 def main():
-    print("Hello World")
-    print(searchRepos())
+    # Authenticate
+    g = Github(ACCESS_TOKEN)
+
+    # Test authentication success
+    user = g.get_user()
+    print(user.login)
+    
+    config_file = open(config_file_name)
+    config_data = json.load(config_file)
+    code_query = config_data['code_query']
+
+    # Get repo info based on query
+    repositories = g.search_code(query=code_query)
+    for repo in repositories:
+        owner = repo.repository.owner
+        print(owner.name, owner.location, owner.bio, owner.role, )
 
 if __name__ == "__main__":
     main()
