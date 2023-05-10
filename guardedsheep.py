a412288a59af3d3c35d398fd93563d7b61827b0f
George Tipton <gtip22@gmail.com>
2023-05-10 14:07:06 -0600

    searchRepos init

--- guardedsheep.py
+++ guardedsheep.py
@@ -1,11 +1,11 @@
 from github import Github
 
-ACCESS_TOKEN = 'put your token here'
-
-g = Github(ACCESS_TOKEN)
+def searchRepos():
+    return "Ayo"
 
 def main():
     print("Hello World")
+    print(searchRepos())
 
 if __name__ == "__main__":
     main()
